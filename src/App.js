f922a67a17b9f4cd9c352c50d25dcb85d71f5cd4
Oksana Nedilko <nedilko7b@gmail.com>
2023-02-03 16:40:09 +0200

    added info blobs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import s from './App.module.css';
 import Password from './components/Password';
+import basicBackground from './img/basicBackground.png';
+
 
 function App() {
   return (
-    <div className={s.App}>
+    <div className={s.App} style={{ backgroundImage: `url(${basicBackground})` }}>
+      <div className={s.headerBlob}>
+        <p className={s.headerBlobText}>Test{'\n'}password{'\n'}strength</p>
+      </div>
       <Password />
+      <div className={s.descriptionBlob}>
+        <div className={s.descriptionBlobText}>
+          Rules
+          <p>Only letters/digits/symbols - the password is easy;{'\n'}
+            Combination of letters-symbols/letters-digits/digits-symbols - the password is medium;{'\n'}
+            Has letters, symbols and numbers - the password is strong;
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
